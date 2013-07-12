04481479a22b6e9120678d1310294770b9a68bab
reaperes <emerald105@hanmail.net>
2013-07-12 15:40:49 +0900

    1-4

---
+++ test/Pawn.java
@@ -0,0 +1,11 @@
+
+public class Pawn {
+	String color="white";
+	
+	String askColor() {
+		// TODO Auto-generated method stub
+		
+		return color;
+	}
+
+}
--- test/PawnTest.java
+++ test/PawnTest.java
@@ -3,6 +3,8 @@ import junit.framework.TestCase;
 
 public class PawnTest extends TestCase {
 	public void testCreate() throws Exception {
-		
+		Pawn pawn = new Pawn();
+		String pawnColor = "white";
+		assertEquals(pawnColor, pawn.askColor());
 	}
 }
